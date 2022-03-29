e465c284d2063fc010dc21c065c5762b5b26c538
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-03-29 14:40:51 +0700

    .

---
+++ database.sql
@@ -0,0 +1,13 @@
+﻿create database QLSach
+go
+use QLSach
+go
+create table Sach(
+	MaSach int identity primary key,
+	TenSach nvarchar(50) not null,
+	LinhVu nvarchar(50) not null,
+	CacTacGia nvarchar(200) not null,
+	NhaXB nvarchar(50) not null,
+	NamXB int,
+	LanTaiBan int
+)
